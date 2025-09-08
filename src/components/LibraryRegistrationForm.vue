f003c5cc91e9035a8f183cb5b18f0e14ae3c69c1
wlin0057 <wlin0057@student.monash.edu>
2025-09-08 18:09:58 +1000

    eFolio week5: change header

--- src/components/LibraryRegistrationForm.vue
+++ src/components/LibraryRegistrationForm.vue
@@ -90,7 +90,7 @@ const validateConfirmPassword = (blur) => {
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-8 offset-md-2">
-        <h1 class="text-center">🗄️ W4. Library Registration Form</h1>
+        <h1 class="text-center">🗄️ W5. Library Registration Form</h1>
         <p class="text-center">
           This form now includes validation. Registered users are displayed in a data table below
           (PrimeVue).
