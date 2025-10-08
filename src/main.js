6b2f4fe6787224b970d7dedab19416de9c1989b8
wlin0057 <wlin0057@student.monash.edu>
2025-10-08 16:52:05 +1100

    Added Firebase Authentication for week7 efolio

--- src/main.js
+++ src/main.js
@@ -10,6 +10,19 @@ import Aura from '@primevue/themes/aura'
 
 import router from './router'
 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAaprVEIPJQEJbffwTxWkAa3ExeUd2rjLc',
+  authDomain: 'week7-wenling.firebaseapp.com',
+  projectId: 'week7-wenling',
+  storageBucket: 'week7-wenling.firebasestorage.app',
+  messagingSenderId: '362969873861',
+  appId: '1:362969873861:web:17ad44d863b1301a5edfac'
+}
+
+initializeApp(firebaseConfig)
+
 const app = createApp(App)
 
 app.use(router)
