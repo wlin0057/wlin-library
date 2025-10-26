46b7b726d805040b74155b4fdcb3a9d4ed9d98cb
wlin0057 <wlin0057@student.monash.edu>
2025-10-27 00:16:41 +1100

    update http to https

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -37,7 +37,7 @@ export default {
     },
     iconUrl() {
       return this.weatherData
-        ? `http://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+        ? `https://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
         : null
     }
   },
@@ -49,13 +49,13 @@ export default {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async (position) => {
           const { latitude, longitude } = position.coords
-          const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`
           await this.fetchWeatherData(url)
         })
       }
     },
     async searchByCity() {
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`
       await this.fetchWeatherData(url)
     },
     async fetchWeatherData(url) {
